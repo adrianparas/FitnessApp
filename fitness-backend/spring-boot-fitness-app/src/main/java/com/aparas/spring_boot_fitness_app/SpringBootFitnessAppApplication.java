4a4d75ffdcb3979c607cf0b910ac6333bb095eb3
Adrian Paras <aparas@terpmail.umd.edu>
2025-02-05 10:33:54 -0600

    setting up backend directory

---
+++ fitness-backend/spring-boot-fitness-app/src/main/java/com/aparas/spring_boot_fitness_app/SpringBootFitnessAppApplication.java
@@ -0,0 +1,13 @@
+package com.aparas.spring_boot_fitness_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFitnessAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFitnessAppApplication.class, args);
+	}
+
+}
